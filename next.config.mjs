bf8b0187e94a77a1c6444894208c0b52b9473749
Fajley Rabbi <fajleyrabbi22@gmail.com>
2026-05-21 23:16:24 +0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  // Suppress hydration warnings for dark mode class on <html>
+  reactStrictMode: true,
+};
+
+export default nextConfig;
